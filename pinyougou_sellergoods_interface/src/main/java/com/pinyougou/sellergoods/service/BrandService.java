449d3b73242f8be9d11caba0470c9820ee79cb89
lihai <171045060@qq.com>
2021-02-18 11:22:30 +0800

    Initial commit

---
+++ pinyougou_sellergoods_interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,13 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * @author lihai
+ * @since 2021/2/16 0016
+ */
+public interface BrandService {
+    List<TbBrand> findAll();
+}
